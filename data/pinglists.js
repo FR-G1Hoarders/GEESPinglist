d7d3644420527c4a1051941c62d8d7f9e8379d95
BlueLatios <23615818+Blue-Latios@users.noreply.github.com>
2021-11-27 23:01:20 +0700

    Spreadsheet links change
    Test version to see whether the pinglist results change

--- data/pinglists.js
+++ data/pinglists.js
@@ -1,5 +1,5 @@
 module.exports = {
-  GENERAL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS052ICFHQ2-QnBrPbxwzCsyrAB5w3hF-vUhQTyovsRccReH3w0KaONfEMhvkLqI3EjkDgQOfdnFPCQ/pub?gid=1463339657&single=true&output=csv',
-  DATES: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS052ICFHQ2-QnBrPbxwzCsyrAB5w3hF-vUhQTyovsRccReH3w0KaONfEMhvkLqI3EjkDgQOfdnFPCQ/pub?gid=595635247&single=true&output=csv',
-  SPECIFICS: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS052ICFHQ2-QnBrPbxwzCsyrAB5w3hF-vUhQTyovsRccReH3w0KaONfEMhvkLqI3EjkDgQOfdnFPCQ/pub?gid=965246332&single=true&output=csv',
+  GENERAL: 'https://docs.google.com/spreadsheets/d/1H6hchGjg3VSRglWJz15lYFmq7VSTpWmkUOmIBIgA73o/export?format=csv&id=1H6hchGjg3VSRglWJz15lYFmq7VSTpWmkUOmIBIgA73o&gid=1463339657',
+  DATES: 'https://docs.google.com/spreadsheets/d/1H6hchGjg3VSRglWJz15lYFmq7VSTpWmkUOmIBIgA73o/export?format=csv&id=1H6hchGjg3VSRglWJz15lYFmq7VSTpWmkUOmIBIgA73o&gid=595635247',
+  SPECIFICS: 'https://docs.google.com/spreadsheets/d/1H6hchGjg3VSRglWJz15lYFmq7VSTpWmkUOmIBIgA73o/export?format=csv&id=1H6hchGjg3VSRglWJz15lYFmq7VSTpWmkUOmIBIgA73o&gid=965246332',
 };
